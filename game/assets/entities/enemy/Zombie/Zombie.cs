b7f9b5f0b03229c476a8fb24f169ab0aeffdf8a5
Tombleron <fokindacer@gmail.com>
2023-12-04 22:42:56 +0700

    AAOAOAO FIRERERE

--- game/assets/entities/enemy/Zombie/Zombie.cs
+++ game/assets/entities/enemy/Zombie/Zombie.cs
@@ -4,50 +4,50 @@ using System.Threading.Tasks;
 
 public partial class Zombie : Enemy
 {
-    [Export] private float _chargeSpeed;
+	[Export] private float _chargeSpeed;
 
-    // Almost base StartAttack but with velocity reset.
-    protected override async void StartAttack()
-    {
-        BlockStateMachine = true;
+	// Almost base StartAttack but with velocity reset.
+	protected override async void StartAttack()
+	{
+		BlockStateMachine = true;
 
-        var timePassedSeconds = 0f;
-        var stepMs = 50;
-        while (timePassedSeconds < StartAttackTimeSeconds)
-        {
-            await Task.Delay(stepMs);
-            timePassedSeconds += stepMs / 1000f;
+		var timePassedSeconds = 0f;
+		var stepMs = 50;
+		while (timePassedSeconds < StartAttackTimeSeconds)
+		{
+			await Task.Delay(stepMs);
+			timePassedSeconds += stepMs / 1000f;
 
-            if (Target.GlobalPosition.DistanceTo(GlobalPosition) > AttackRange)
-            {
-                BlockStateMachine = false;
-                State = EnemyState.Chase;
-                return;
-            }
-            
-            Velocity = Vector3.Zero;
-            MoveAndSlide();
-        }
-        
-        BlockStateMachine = false;
-        State = EnemyState.Attack;
-    }
+			if (Target.GlobalPosition.DistanceTo(GlobalPosition) > AttackRange)
+			{
+				BlockStateMachine = false;
+				State = EnemyState.Chase;
+				return;
+			}
+			
+			Velocity = Vector3.Zero;
+			MoveAndSlide();
+		}
+		
+		BlockStateMachine = false;
+		State = EnemyState.Attack;
+	}
 
-    protected override async void AttackInternal()
-    {
-        HitArea.Monitoring = true;
-        
+	protected override async void AttackInternal()
+	{
+		HitArea.Monitoring = true;
+		
 
-        Vector3 direction = Target.GlobalPosition - GlobalPosition;
+		Vector3 direction = Target.GlobalPosition - GlobalPosition;
 
-        direction.Y = 0.05f;
-        direction = direction.Normalized();
-        
-        ApplyImpulse(direction * _chargeSpeed);
+		direction.Y = 0.05f;
+		direction = direction.Normalized();
+		
+		ApplyImpulse(direction * _chargeSpeed);
 
-        State = EnemyState.EndAttack;
-        
-        await Task.Delay((int)EndAttackTimeSeconds * 1000);
-        HitArea.Monitoring = false;
-    }
+		State = EnemyState.EndAttack;
+		
+		await Task.Delay((int)EndAttackTimeSeconds * 1000);
+		HitArea.Monitoring = false;
+	}
 }
--- game/assets/entities/enemy/boss/Boss.cs
+++ game/assets/entities/enemy/boss/Boss.cs
@@ -4,175 +4,175 @@ using System.Threading.Tasks;
 
 public partial class Boss : Enemy
 {
-    [Export] private PackedScene _bossProjectile;
-    
-    [Export] private PackedScene _horcruxScene;
-    [Export] private PackedScene[] _enemyScenes;
-    
-    [Export] private Node3D[] _horcruxSpawnPositions;
-    [Export] private EnemySpawner[] _enemySpawners;
+	[Export] private PackedScene _bossProjectile;
+	
+	[Export] private PackedScene _horcruxScene;
+	[Export] private PackedScene[] _enemyScenes;
+	
+	[Export] private Node3D[] _horcruxSpawnPositions;
+	[Export] private EnemySpawner[] _enemySpawners;
 
-    [Export] private HitboxComponent _hitboxComponent;
+	[Export] private HitboxComponent _hitboxComponent;
 
-    [Export] private uint _horcruxSpawnCount;
-    [Export] private uint _monsterSpawnCount;
-    
-    [Export] private float _horcruxSpawnCooldownSeconds;
-    [Export] private float _monsterSpawnCooldownSeconds;
-    
-    private bool _canSpawnHorcrux = true;
-    private bool _canSpawnMonsters = true;
-    
-    private uint _horcruxLeft;
+	[Export] private uint _horcruxSpawnCount;
+	[Export] private uint _monsterSpawnCount;
+	
+	[Export] private float _horcruxSpawnCooldownSeconds;
+	[Export] private float _monsterSpawnCooldownSeconds;
+	
+	private bool _canSpawnHorcrux = true;
+	private bool _canSpawnMonsters = true;
+	
+	private uint _horcruxLeft;
 
-    public override void _Ready()
-    {
-        for (var i = 0; i < _enemySpawners.Length; i++)
-        {
-            EnemySpawner spawner = _enemySpawners[i];
-            spawner.Position = _horcruxSpawnPositions[i % _horcruxSpawnPositions.Length].Position;
-        }
-    }
-    
-    public override void _PhysicsProcess(double delta)
-    {
-        if (BlockStateMachine == true)
-        {
-            return;
-        }
+	public override void _Ready()
+	{
+		for (var i = 0; i < _enemySpawners.Length; i++)
+		{
+			EnemySpawner spawner = _enemySpawners[i];
+			spawner.Position = _horcruxSpawnPositions[i % _horcruxSpawnPositions.Length].Position;
+		}
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		if (BlockStateMachine == true)
+		{
+			return;
+		}
 
-        switch (State)
-        {
-            case EnemyState.Idle:
-                Idle();
-                break;
-            case EnemyState.StartAttack:
-                StartAttack();
-                break;
-            case EnemyState.Attack:
-                Attack();
-                break;
-            case EnemyState.EndAttack:
-                EndAttack();
-                break;
-            case EnemyState.SpawnHorcrux:
-                SpawnHorcrux();
-                break;
-            case EnemyState.SpawnMonsters:
-                SpawnMonsters();
-                break;
-            case EnemyState.Dead: // Do nothing.
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
+		switch (State)
+		{
+			case EnemyState.Idle:
+				Idle();
+				break;
+			case EnemyState.StartAttack:
+				StartAttack();
+				break;
+			case EnemyState.Attack:
+				Attack();
+				break;
+			case EnemyState.EndAttack:
+				EndAttack();
+				break;
+			case EnemyState.SpawnHorcrux:
+				SpawnHorcrux();
+				break;
+			case EnemyState.SpawnMonsters:
+				SpawnMonsters();
+				break;
+			case EnemyState.Dead: // Do nothing.
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
 
-    protected override void Idle()
-    {
-        if (_canSpawnMonsters == true)
-        {
-            State = EnemyState.SpawnMonsters;
-            return;
-        }
-        
-        if (_canSpawnHorcrux == true)
-        {
-            State = EnemyState.SpawnHorcrux;
-            return;
-        }
+	protected override void Idle()
+	{
+		if (_canSpawnMonsters == true)
+		{
+			State = EnemyState.SpawnMonsters;
+			return;
+		}
+		
+		if (_canSpawnHorcrux == true)
+		{
+			State = EnemyState.SpawnHorcrux;
+			return;
+		}
 
-        if (IsAttackOnCooldown == false)
-        {
-            State = EnemyState.StartAttack;
-        }
-    }
+		if (IsAttackOnCooldown == false)
+		{
+			State = EnemyState.StartAttack;
+		}
+	}
 
-    #region StateMachine
+	#region StateMachine
 
-    private void SpawnHorcrux()
-    {
-        GD.Print("Boss is invulnerable.");
-        _hitboxComponent.SetDeferred("monitorable", false);
+	private void SpawnHorcrux()
+	{
+		GD.Print("Boss is invulnerable.");
+		_hitboxComponent.SetDeferred("monitorable", false);
 
-        _canSpawnHorcrux = false;
+		_canSpawnHorcrux = false;
 
-        for (var i = 0; i < _horcruxSpawnCount; i++)
-        {
-            Horcrux horcrux = _horcruxScene.Instantiate() as Horcrux;
+		for (var i = 0; i < _horcruxSpawnCount; i++)
+		{
+			Horcrux horcrux = _horcruxScene.Instantiate() as Horcrux;
 
-            if (horcrux == null)
-            {
-                return;
-            }
+			if (horcrux == null)
+			{
+				return;
+			}
 
-            _horcruxLeft++;
-            AddChild(horcrux);
-            horcrux.Position = _horcruxSpawnPositions[i].Position;
-            horcrux.TopLevel = true;
-        }
-        
-        Horcrux.OnHorcruxDie += OnHorcruxDie;
-        
-        State = EnemyState.Idle;
-    }
+			_horcruxLeft++;
+			AddChild(horcrux);
+			horcrux.Position = _horcruxSpawnPositions[i].Position;
+			horcrux.TopLevel = true;
+		}
+		
+		Horcrux.OnHorcruxDie += OnHorcruxDie;
+		
+		State = EnemyState.Idle;
+	}
 
-    private async void SpawnMonsters()
-    {
-        _canSpawnMonsters = false;
-        
-        for (var i = 0; i < _monsterSpawnCount; i++)
-        {
-            PackedScene randomEnemy = _enemyScenes[GD.Randi() % _enemyScenes.Length];
-            _enemySpawners[i % _enemySpawners.Length].Spawn(randomEnemy, Target);
-        }
-        
-        State = EnemyState.Idle;
-        await Task.Delay((int)(_monsterSpawnCooldownSeconds * 1000));
-        _canSpawnMonsters = true;
-    }
+	private async void SpawnMonsters()
+	{
+		_canSpawnMonsters = false;
+		
+		for (var i = 0; i < _monsterSpawnCount; i++)
+		{
+			PackedScene randomEnemy = _enemyScenes[GD.Randi() % _enemyScenes.Length];
+			_enemySpawners[i % _enemySpawners.Length].Spawn(randomEnemy, Target);
+		}
+		
+		State = EnemyState.Idle;
+		await Task.Delay((int)(_monsterSpawnCooldownSeconds * 1000));
+		_canSpawnMonsters = true;
+	}
 
-    protected override void AttackInternal()
-    {
-        BossProjectile bossProjectile = _bossProjectile.Instantiate() as BossProjectile;
-        
-        if (bossProjectile == null)
-        {
-            return;
-        }
+	protected override void AttackInternal()
+	{
+		BossProjectile bossProjectile = _bossProjectile.Instantiate() as BossProjectile;
+		
+		if (bossProjectile == null)
+		{
+			return;
+		}
 
-        GetTree().Root.AddChild(bossProjectile);
-        bossProjectile.GlobalPosition = GlobalPosition + RealPosition;
-        bossProjectile.SetDirection(Target.GlobalPosition + Vector3.Up - bossProjectile.GlobalPosition);
-        bossProjectile.Cast();
-    }
-    
-    // TODO: Can be removed if can be switched to Idle in Enemy class?
-    protected override async void EndAttack()
-    {
-        BlockStateMachine = true;
-        await Task.Delay((int)(EndAttackTimeSeconds * 1000));
-        BlockStateMachine = false;
+		GetTree().Root.AddChild(bossProjectile);
+		bossProjectile.GlobalPosition = GlobalPosition + RealPosition;
+		bossProjectile.SetDirection(Target.GlobalPosition + Vector3.Up - bossProjectile.GlobalPosition);
+		bossProjectile.Cast();
+	}
+	
+	// TODO: Can be removed if can be switched to Idle in Enemy class?
+	protected override async void EndAttack()
+	{
+		BlockStateMachine = true;
+		await Task.Delay((int)(EndAttackTimeSeconds * 1000));
+		BlockStateMachine = false;
 
-        State = EnemyState.Idle; // TODO: Can be switched to Idle?
-    }
+		State = EnemyState.Idle; // TODO: Can be switched to Idle?
+	}
 
-    #endregion
+	#endregion
 
-    private async void OnHorcruxDie()
-    {
-        _horcruxLeft--;
+	private async void OnHorcruxDie()
+	{
+		_horcruxLeft--;
 
-        if (_horcruxLeft != 0)
-        {
-            return;
-        }
-        
-        Horcrux.OnHorcruxDie -= OnHorcruxDie;
+		if (_horcruxLeft != 0)
+		{
+			return;
+		}
+		
+		Horcrux.OnHorcruxDie -= OnHorcruxDie;
 
-        GD.Print("Boss is vulnerable.");
-        _hitboxComponent.SetDeferred("monitorable", true);
-        await Task.Delay((int)(_horcruxSpawnCooldownSeconds * 1000));
-        _canSpawnHorcrux = true;
-    }
+		GD.Print("Boss is vulnerable.");
+		_hitboxComponent.SetDeferred("monitorable", true);
+		await Task.Delay((int)(_horcruxSpawnCooldownSeconds * 1000));
+		_canSpawnHorcrux = true;
+	}
 }
