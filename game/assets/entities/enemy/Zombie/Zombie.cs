0918e9d47acb4d5c0dfaed1e7b5b46b59ffe4d1f
TheActualTwinkle <super.irums2@gmail.com>
2023-12-04 13:34:31 +0700

    Fix: zombie charge

--- game/assets/entities/enemy/Zombie/Zombie.cs
+++ game/assets/entities/enemy/Zombie/Zombie.cs
@@ -4,14 +4,15 @@ using System.Threading.Tasks;
 
 public partial class Zombie : Enemy
 {
-    [Export] private float _chargeTime;
     [Export] private float _chargeSpeed;
 
-    protected override async void Attack()
+    protected override async void StartAttack()
     {
+        BlockStateMachine = true;
+
         var timePassedSeconds = 0f;
         var stepMs = 50;
-        while (timePassedSeconds < _chargeTime)
+        while (timePassedSeconds < StartAttackTimeSeconds)
         {
             await Task.Delay(stepMs);
             timePassedSeconds += stepMs / 1000f;
@@ -19,15 +20,25 @@ public partial class Zombie : Enemy
             Velocity = Vector3.Zero;
             MoveAndSlide();
         }
+        
+        BlockStateMachine = false;
+        State = EnemyState.Attack;
+    }
+
+    protected override async void AttackInternal()
+    {
+        HitArea.Monitoring = true;
 
         Vector3 direction = Target.GlobalPosition - GlobalPosition;
 
         direction.Y = 0.05f;
         direction = direction.Normalized();
         
-        AttackInternal();
         ApplyImpulse(direction * _chargeSpeed);
 
         State = EnemyState.EndAttack;
+        
+        await Task.Delay((int)EndAttackTimeSeconds * 1000);
+        HitArea.Monitoring = false;
     }
 }
