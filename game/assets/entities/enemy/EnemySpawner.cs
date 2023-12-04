16aa1a8d209be794a6c18481852bc6cdea12dcfa
TheActualTwinkle <super.irums2@gmail.com>
2023-12-04 16:20:19 +0700

    Add: Mr. Door that will be opened after N count of enemyll be killed. Add: Auto mob spawner.

--- game/assets/entities/enemy/EnemySpawner.cs
+++ game/assets/entities/enemy/EnemySpawner.cs
@@ -1,8 +1,26 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class EnemySpawner : Node3D
 {
+    [Export] private PackedScene _enemyScene;
+    
+    [Export] private bool _autoSpawn;
+    [Export] private float _spawnIntervalSeconds;
+
+    [Export] private string _pathToPlayerInScene; // Path to player RELATIVE to this node!!! SANYAAAAAAAAAAAAAAAAAAAAAAA
+
+    public override void _Ready()
+    {
+        if (_autoSpawn == false)
+        {
+            return;
+        }
+
+        AutoSpawn();
+    }
+
     public void Spawn(PackedScene enemyScene, Player target)
     {
         Enemy enemy = enemyScene.Instantiate() as Enemy;
@@ -15,5 +33,14 @@ public partial class EnemySpawner : Node3D
         GetTree().Root.AddChild(enemy);
         enemy.GlobalPosition = GlobalPosition;
         enemy.SetTarget(target);
+    }
+
+    private async void AutoSpawn()
+    {
+        while (true)
+        {
+            Spawn(_enemyScene, GetParent().GetNode<Player>(_pathToPlayerInScene));
+            await Task.Delay((int)(_spawnIntervalSeconds * 1000));
+        }
     }
 }
