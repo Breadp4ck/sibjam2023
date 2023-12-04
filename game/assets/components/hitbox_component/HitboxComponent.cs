416e6207211b540050e3c239b0462fe805ab31fc
Ivan Danchenko <danchenko.ivan@inbox.ru>
2023-12-04 20:03:32 +0700

    Update: HP progress bar

--- game/assets/components/hitbox_component/HitboxComponent.cs
+++ game/assets/components/hitbox_component/HitboxComponent.cs
@@ -1,11 +1,15 @@
+using System;
 using Godot;
 
 public partial class HitboxComponent : Area3D
 {
+	public event Action<double> HPLostEvent;
+    
     [Export] private HealthComponent _healthComponent;
     
     public void Hit(double damage)
     {
         _healthComponent.Hit(damage);
+        HPLostEvent?.Invoke(damage);
     }
 }
